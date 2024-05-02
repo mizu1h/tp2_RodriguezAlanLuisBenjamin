2079c0ae364fc20bfea755779635aaca183104e3
PC <PC@DESKTOP-165AL12>
2024-05-02 14:10:22 -0300

    entrega del punto2

---
+++ src/ar/edu/unju/fi/ejercicio02/constantes/Constantes.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.ejercicio02.constantes;
+
+public class Constantes {
+	public enum Mes{
+		ENERO, FEBRERO, MARZO, ABRIL, MAYO, JUNIO, JULIO, AGOSTO, SEPTIEMBRE, OCTUBRE, NOVIEMBRE,DICIEMBRE
+	}
+	private int mes;
+	public int getMes() {
+		return mes;
+	}
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+	}
