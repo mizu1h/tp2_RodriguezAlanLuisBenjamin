0c7ece86d44288807026a3c08527d686d21da398
PC <PC@DESKTOP-165AL12>
2024-05-06 23:31:32 -0300

    punto 6

---
+++ src/ar/edu/unju/fi/ejercicio06/model/FelinoDomestico.java
@@ -0,0 +1,28 @@
+package ar.edu.unju.fi.ejercicio06.model;
+
+public class FelinoDomestico {
+	private String nombre;
+    private byte edad;
+    private float peso;
+    public FelinoDomestico(String nombre, byte edad, float peso) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.peso = peso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public byte getEdad() {
+        return edad;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+    @Override
+    public String toString() {
+        return "Felino Doméstico [nombre=" + nombre + ", edad=" + edad + ", peso(kg)=" + peso + "]";
+    }
+}
